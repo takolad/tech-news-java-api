3c3a2cdc903e2738d495d0096da4d0ca50272efe
John Hinojosa <hinojohn@ymail.com>
2021-08-09 16:54:24 -0500

    added missing dependency

--- src/main/java/com/technews/model/User.java
+++ src/main/java/com/technews/model/User.java
@@ -3,6 +3,7 @@ package com.technews.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
